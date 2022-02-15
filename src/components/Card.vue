63a7031aadb861e0a8cf774c79fcac5c44e67cbf
Gioelinz <gioelerunza.gr@gmail.com>
2022-02-15 11:14:48 +0100

    add posters on Card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="container-card">
+    <img :src="assignImgPoster" :alt="item.title || item.name" />
     <h2>{{ item.title || item.name }}</h2>
     <h3>({{ item.original_title || item.original_name }})</h3>
     <img
       :src="assignImgLanguage(item.original_language)"
       width="60"
       height="40"
-      :alt="item.title || item.name"
+      :alt="item.original_language"
       :title="item.original_language"
     />
     <p>{{ item.vote_average }}</p>
@@ -32,6 +33,11 @@ export default {
           break;
       }
       return flag;
+    },
+  },
+  computed: {
+    assignImgPoster() {
+      return `https://image.tmdb.org/t/p/w342/${this.item.poster_path}`;
     },
   },
 };
